c598a402cc0e1152d11b65e4e49aa2194d3a0777
kaanmertkoc <46743238+kaanmertkoc@users.noreply.github.com>
2021-06-24 01:01:32 +0300

    Showcase component and cand custom 404 page implement

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,12 @@
 import Head from 'next/head';
-import styles from '../styles/Layout.module.css';
+import { useRouter } from 'next/router';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import Showcase from '@/components/Showcase';
+import styles from '@/styles/Layout.module.css';
+
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -8,7 +14,10 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
+      <Header />
+      {router.pathname === '/' && <Showcase />}
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }

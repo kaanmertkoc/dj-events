908aa4ec0c13c044b69ec3c6f259b5065235c457
kaanmertkoc <46743238+kaanmertkoc@users.noreply.github.com>
2021-07-16 00:20:34 +0300

    Login, register form create

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import Link from "next/link";
 import styles from "@/styles/Header.module.css";
 import Search from "@/components/Search.js";
@@ -21,6 +22,13 @@ export default function Header() {
           <li>
             <Link href="/events/add">
               <a>Add Event</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/account/login">
+              <a className="btn-secondary btn-icon">
+                <FaSignInAlt /> Login
+              </a>
             </Link>
           </li>
         </ul>
